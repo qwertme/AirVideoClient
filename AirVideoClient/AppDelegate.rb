0d43703e939692f47eb1bc426918ea3e3f3f8cd3
Eric di Domenico <eric@didomenico.com>
2012-04-20 16:55:29 -0700

    Initial Commit

---
+++ AirVideoClient/AppDelegate.rb
@@ -0,0 +1,15 @@
+#
+#  AppDelegate.rb
+#  AirVideoClient
+#
+#  Created by edomenico on 4/20/12.
+#  Copyright 2012 __MyCompanyName__. All rights reserved.
+#
+
+class AppDelegate
+  attr_accessor :window
+  def applicationDidFinishLaunching(a_notification)
+    # Insert code here to initialize your application
+  end
+end
+
---
+++ AirVideoClient/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AirVideoClient
+//
+//  Created by edomenico on 4/20/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+#import <MacRuby/MacRuby.h>
+
+int main(int argc, char *argv[])
+{
+  return macruby_main("rb_main.rb", argc, argv);
+}
