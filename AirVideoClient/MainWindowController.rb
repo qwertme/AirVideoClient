2b8fdbaee4226771347c983740f56a45d2911f5d
Eric di Domenico <eric@didomenico.com>
2012-04-21 14:44:29 -0700

    Implement password box for server

--- AirVideoClient/MainWindowController.rb
+++ AirVideoClient/MainWindowController.rb
@@ -10,12 +10,16 @@ require 'airvideo'
 
 class MainWindowController < NSWindowController
   attr_accessor :browser
+  attr_accessor :server_box
+  attr_accessor :server_name
+  attr_accessor :server_password
   
   def initialize
     @net_browser = NSNetServiceBrowser.alloc.init
     @net_browser.delegate = self
     @net_browser.searchForServicesOfType('_airvideoserver._tcp', inDomain:'local')
     @servers = []
+    @connection_cache = {}
     @cache = {}
   end
   
@@ -23,24 +27,33 @@ class MainWindowController < NSWindowController
     initialize
   end
   
+  def connectToServer(sender)
+    @connection_cache[server_name.stringValue] = AirVideo::Client.new("#{server_name.stringValue}.local", 45631, server_password.stringValue)
+    server_box.hidden = true
+    browser.loadColumnZero
+  end
+  
   def rootItemForBrowser(browser)
     @net_browser
   end
   
-  
   def browser(browser, numberOfChildrenOfItem:item) 
     if item == @net_browser
       return @servers.size
+    elsif item.is_a?(NSNetService) && !@connection_cache.has_key?(item.name)
+      server_box.hidden = false
+      server_name.stringValue = item.name
+      return 0
     elsif !@cache.has_key?(item)
-      if item.is_a?(NSNetService)
-        server = AirVideo::Client.new("#{item.name}.local",45631,'asd092')
-        @cache[item] = server.ls
+      if item.is_a?(NSNetService) && @connection_cache.has_key?(item.name)
+        @cache[item] = @connection_cache[item.name].ls 
       elsif item.is_a?(AirVideo::Client::FolderObject)
         @cache[item] = item.ls
       else
         @cache[item] = []
       end
     end
+    
     return @cache[item].count
   end
   
@@ -63,7 +76,6 @@ class MainWindowController < NSWindowController
   def netServiceBrowser(netServiceBrowser, didFindService:netService, moreComing:moreDomainsComing)
     @servers << netService
     browser.loadColumnZero
-    puts netService.name
   end
   
 end
