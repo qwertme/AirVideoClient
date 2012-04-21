aec3b927e1e2e3c2d94ff99515e3de4fd179d337
Eric di Domenico <eric@didomenico.com>
2012-04-21 16:45:37 -0700

    Allow playing video

--- AirVideoClient/MainWindowController.rb
+++ AirVideoClient/MainWindowController.rb
@@ -13,6 +13,9 @@ class MainWindowController < NSWindowController
   attr_accessor :server_box
   attr_accessor :server_name
   attr_accessor :server_password
+  attr_accessor :video_box
+  attr_accessor :video_name
+  attr_accessor :video_link
   
   def initialize
     @net_browser = NSNetServiceBrowser.alloc.init
@@ -25,12 +28,33 @@ class MainWindowController < NSWindowController
   
   def awakeFromNib
     initialize
+    browser.columnResizingType = NSBrowserUserColumnResizing
   end
   
   def connectToServer(sender)
     @connection_cache[server_name.stringValue] = AirVideo::Client.new("#{server_name.stringValue}.local", 45631, server_password.stringValue)
     server_box.hidden = true
     browser.loadColumnZero
+  end
+  
+  def browserClicked(sender)
+    item = browser.itemAtIndexPath(browser.selectionIndexPath)
+
+    if item.is_a?(NSNetService) && !@connection_cache.has_key?(item.name)
+      server_box.hidden = false
+      server_name.stringValue = item.name
+    elsif item.is_a?(AirVideo::Client::VideoObject)
+      video_box.hidden = false
+      video_name.stringValue = item.name
+      video_link.stringValue = item.url
+    else
+      server_box.hidden = true
+      video_box.hidden = true
+    end
+  end
+  
+  def openInQuickTime(sender)
+    `open -a "QuickTime Player" #{video_link.stringValue}`
   end
   
   def rootItemForBrowser(browser)
@@ -40,21 +64,15 @@ class MainWindowController < NSWindowController
   def browser(browser, numberOfChildrenOfItem:item) 
     if item == @net_browser
       return @servers.size
-    elsif item.is_a?(NSNetService) && !@connection_cache.has_key?(item.name)
-      server_box.hidden = false
-      server_name.stringValue = item.name
-      return 0
     elsif !@cache.has_key?(item)
       if item.is_a?(NSNetService) && @connection_cache.has_key?(item.name)
         @cache[item] = @connection_cache[item.name].ls 
       elsif item.is_a?(AirVideo::Client::FolderObject)
         @cache[item] = item.ls
-      else
-        @cache[item] = []
       end
     end
     
-    return @cache[item].count
+    return @cache.has_key?(item) ? @cache[item].count : 0
   end
   
   def browser(browser, child:index, ofItem:item)
